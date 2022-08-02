be44770945ef02fe988af2dfd5f61c42fcda5eb1
Marcel Wagenlaender <marcel.wagenlander19@imperial.ac.uk>
2022-08-02 11:01:36 +0000

    add a script that generate a rank map

---
+++ gen_rank_map.py
@@ -0,0 +1,40 @@
+import json
+import os
+import re
+
+
+def main():
+    framework = 'megatron-lm'
+    pp_size = 2
+    mp_size = 2
+    dp_size = 1
+    total_size = pp_size * mp_size * dp_size
+    direc = f'{framework}/pp{pp_size:02d}/mp{mp_size:02d}/dp{dp_size:02d}'
+
+    mapping = dict()
+
+    for rank in range(total_size):
+        rank_dir = f'{direc}/rank{rank:02d}'
+        for entry in os.scandir(rank_dir):
+            if pp_size > 1:
+                pattern = r'mp_rank_(\d+)_(\d+)'
+            else:
+                pattern = r'mp_rank_(\d+)'
+            ma = re.match(pattern, entry.name)
+            if ma is None:
+                print('error')
+                return
+            mp_rank = int(ma.group(1))
+            if pp_size > 1:
+                pp_rank = int(ma.group(2))
+                mapping[rank] = {'mp_rank': mp_rank, 'pp_rank': pp_rank}
+            else:
+                mapping[rank] = {'mp_rank': mp_rank}
+            break
+
+    with open(f'{direc}/rank_map.json', 'w') as json_file:
+        json.dump(mapping, json_file, indent=4)
+
+
+if __name__ == "__main__":
+    main()

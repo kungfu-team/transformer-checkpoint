fd645c8c6221d68b78a75562a598a3a790523d40
Marcel Wagenlaender <marcel.wagenlander19@imperial.ac.uk>
2022-09-04 09:40:51 +0000

    add layer mapping for deepspeed

---
+++ gen_layer_map.py
@@ -0,0 +1,43 @@
+import json
+import os
+import re
+
+
+def main():
+    framework = 'deepspeed'
+    pp_size = 2
+    mp_size = 4
+    dp_size = 1
+    total_size = pp_size * mp_size * dp_size
+    direc = f'{framework}/pp{pp_size:02d}/mp{mp_size:02d}/dp{dp_size:02d}'
+
+    mapping = dict()
+
+    for rank in range(total_size):
+        rank_dir = os.path.join(direc, f'rank{rank:02d}')
+        if not os.path.exists(rank_dir):
+            continue
+
+        layer_numbers = []
+        tp_rank = None
+        for entry in os.scandir(rank_dir):
+            pattern = r'layer_(\d+)-model_(\d+)-model_states.json'
+            mat = re.match(pattern, entry.name)
+            if mat is None:
+                continue
+            layer_num = int(mat.group(1))
+            tp_rank = int(mat.group(2))
+            layer_numbers.append(layer_num)
+
+        if layer_numbers:
+            mapping[rank] = {
+                'tp_rank': tp_rank,
+                'layer_numbers': layer_numbers
+            }
+
+    with open(f'{direc}/layer_map.json', 'w') as json_file:
+        json.dump(mapping, json_file, indent=4)
+
+
+if __name__ == "__main__":
+    main()

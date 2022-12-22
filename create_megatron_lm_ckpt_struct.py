ab49a5f27e424785af4a1da29a1a78c2f59f32fa
Marcel Wagenlaender <marcel.wagenlander19@imperial.ac.uk>
2022-12-22 19:51:14 +0000

    add bert large mp4

--- create_megatron_lm_ckpt_struct.py
+++ create_megatron_lm_ckpt_struct.py
@@ -36,12 +36,12 @@ def create_ckpt_dict(ckpt):
 
 
 def megatron_lm():
-    job_id = "1c7271fa2f"
+    job_id = "0af63fba3e"
     base_dir = os.path.join(os.path.expanduser('~'),
                             f".tenplex/training/{job_id}")
-    size = 2
+    size = 4
     pp = 1
-    mp = 2
+    mp = 4
     dp = size // (pp * mp)
     step = 50
     seq_length = 1024

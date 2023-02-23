24c240894d4e73b25e376079db2134989de08694
Marcel Wagenlaender <marcel.wagenlander19@imperial.ac.uk>
2023-02-23 16:55:40 +0000

    add gpt xl pp4 mp1 dp2

--- gen_model_key_order.py
+++ gen_model_key_order.py
@@ -48,12 +48,12 @@ def find_tensors(value, keys: list = None):
 def main():
     framework = 'megatron-lm'
     model = 'gpt'
-    model_size = 'large'
+    model_size = 'xl'
     # precision = 'fp16'
     # seq_length = 1024
-    pp_size = 8
-    mp_size = 2
-    dp_size = 1
+    pp_size = 4
+    mp_size = 1
+    dp_size = 2
     total_size = pp_size * mp_size * dp_size
     # direc = f'{framework}/{precision}/seq_{seq_length}/pp{pp_size:02d}/mp{mp_size:02d}/dp{dp_size:02d}'
     direc = f'{framework}/{model}/{model_size}/pp{pp_size:02d}/mp{mp_size:02d}/dp{dp_size:02d}'

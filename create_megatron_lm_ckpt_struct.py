b4d14602e6618be5a6fc1fe62e788e467fcd3a60
Marcel Wagenlaender <marcel.wagenlander19@imperial.ac.uk>
2023-04-17 13:17:49 +0000

    add gpt large pp4 mp2 dp1

--- create_megatron_lm_ckpt_struct.py
+++ create_megatron_lm_ckpt_struct.py
@@ -36,11 +36,11 @@ def create_ckpt_dict(ckpt):
 
 
 def megatron_lm():
-    job_id = "0de5fc1f93"
+    job_id = "b82426ef56"
     base_dir = os.path.join(os.path.expanduser('~'),
                             f".tenplex/training/{job_id}")
     size = 8
-    pp = 2
+    pp = 4
     mp = 2
     dp = size // (pp * mp)
     step = 50

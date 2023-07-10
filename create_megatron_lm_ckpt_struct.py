4bc9bb8bb16307ca417b0ac24d28e3bc0e4ca057
Marcel Wagenlaender <marcel.wagenlander19@imperial.ac.uk>
2023-07-10 10:15:00 +0000

    add bert base pp2 mp4 dp1

--- create_megatron_lm_ckpt_struct.py
+++ create_megatron_lm_ckpt_struct.py
@@ -36,12 +36,12 @@ def create_ckpt_dict(ckpt):
 
 
 def megatron_lm():
-    job_id = "e3b18fdbd8"
+    job_id = "e0f11a7ed6"
     base_dir = os.path.join(os.path.expanduser('~'),
                             f".tenplex/training/{job_id}")
     size = 8
     pp = 2
-    mp = 2
+    mp = 4
     dp = size // (pp * mp)
     step = 50
     model = "bert"
